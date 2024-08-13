2b7c6385a0c9810454c0784b3012e3328a1795d1
Michael Kreil <github@michael-kreil.de>
2024-08-13 20:40:50 +0200

    style: make prettier

--- src/lib/BBoxMap/helpers/merge-bboxes.ts
+++ src/lib/BBoxMap/helpers/merge-bboxes.ts
@@ -3,19 +3,18 @@
 import type { BBox } from 'geojson';
 import { readdirSync, readFileSync, writeFileSync } from 'node:fs';
 
-
 type Entry = {
-	label: string,
-	population: number,
-	bbox: BBox,
-}
+	label: string;
+	population: number;
+	bbox: BBox;
+};
 
 process.chdir(import.meta.dirname);
 
 console.log('load data');
 const data: Entry[] = [];
 const knownLabels = new Set<string>();
-readdirSync('sources').forEach(filename => {
+readdirSync('sources').forEach((filename) => {
 	if (!filename.endsWith('.jsonl')) return;
 	const lines = readFileSync('sources/' + filename, 'utf8').split('\n');
 	for (const line of lines) {
@@ -25,26 +24,29 @@ readdirSync('sources').forEach(filename => {
 		if (!entry.population) console.error('Error, population is missing: ' + line);
 
 		if (knownLabels.has(entry.label)) {
-			throw Error(`Error, label "${entry.label}" is duplicated`)
+			throw Error(`Error, label "${entry.label}" is duplicated`);
 		}
 		knownLabels.add(entry.label);
 
 		const bbox = roundBBox(entry.bbox);
 		data.push({ ...entry, bbox });
 	}
-})
+});
 
 data.sort((a, b) => b.population - a.population);
 
 console.log('write data');
-writeFileSync('../bboxes.json',
+writeFileSync(
+	'../bboxes.json',
 	'[\n' +
-	data.map(e => {
-		const r = [e.label, ...e.bbox];
-		if (r.length !== 5) throw Error();
-		return JSON.stringify(r);
-	}).join(',\n')
-	+ '\n]'
+		data
+			.map((e) => {
+				const r = [e.label, ...e.bbox];
+				if (r.length !== 5) throw Error();
+				return JSON.stringify(r);
+			})
+			.join(',\n') +
+		'\n]'
 );
 
 function roundBBox(bbox: BBox): BBox {
@@ -53,11 +55,7 @@ function roundBBox(bbox: BBox): BBox {
 
 	const x = Math.floor(bbox[0] * fx) / fx;
 	const y = Math.floor(bbox[1] * fy) / fy;
-	const b = [
-		x, y,
-		Math.ceil((bbox[2] - x) * fx) / fx,
-		Math.ceil((bbox[3] - y) * fy) / fy,
-	] as BBox;
+	const b = [x, y, Math.ceil((bbox[2] - x) * fx) / fx, Math.ceil((bbox[3] - y) * fy) / fy] as BBox;
 
 	return b;
 
