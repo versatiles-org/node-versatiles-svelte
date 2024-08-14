81ad73cad37d55dc0909274389c36cd2c26bf852
Michael Kreil <github@michael-kreil.de>
2024-08-14 15:03:56 +0200

    feat: improve demo page

--- src/routes/bbox-map/+page.svelte
+++ src/routes/bbox-map/+page.svelte
@@ -2,4 +2,30 @@
 	import { BBoxMap } from '$lib/index.js';
 </script>
 
-<BBoxMap />
+<div class="wrapper" style="color-scheme: light;">
+	<h3>light mode</h3>
+	<div><BBoxMap /></div>
+</div>
+
+<div class="wrapper" style="color-scheme: dark;">
+	<h3>dark mode</h3>
+	<div><BBoxMap /></div>
+</div>
+
+<style>
+	.wrapper {
+		background-color: light-dark(white, black);
+		color: light-dark(black, white);
+		padding: 1em;
+		font-family: sans-serif;
+	}
+	h3 {
+		margin: 0 0 1em 0;
+		text-align: center;
+	}
+	.wrapper > div {
+		width: 80%;
+		height: 30vh;
+		margin: 1em auto;
+	}
+</style>
