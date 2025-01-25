26585e00395eb7a13cf4a42847e0598bf30e642b
Michael Kreil <github@michael-kreil.de>
2025-01-25 17:58:33 +0100

    test: merge_bboxes

---
+++ scripts/bboxes/merge_bboxes.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+import { mergeBBoxes } from './merge_bboxes.js';
+
+describe('mergeBBoxes', () => {
+	it('should merge bounding boxes', async () => {
+		const result = mergeBBoxes();
+		const lines = result.split('\n');
+		for (const line of lines) {
+			const coords = line.replace(/.*?",/, '');
+			if (coords.length > 32) {
+				console.log({ line, coords, coordsLength: coords.length });
+				expect(coords.length).lessThanOrEqual(32);
+			}
+		}
+		const data = JSON.parse(result);
+		expect(data.length).toBeGreaterThan(2000);
+		expect(data[0]).toStrictEqual(['World', -180, -90, 360, 180]);
+	})
+})
