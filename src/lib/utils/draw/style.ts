dd957f4f8cdabf592df6bd47f1b99b062a4ff304
Michael Kreil <github@michael-kreil.de>
2025-01-27 00:47:31 +0100

    feat: prepare style processing

---
+++ src/lib/utils/draw/style.ts
@@ -0,0 +1,37 @@
+import { Color } from '@versatiles/style';
+import type { FillLayerSpecification, LineLayerSpecification } from 'maplibre-gl';
+
+
+export type FillLayerPaint = FillLayerSpecification['paint'];
+export type LineLayerPaint = LineLayerSpecification['paint'];
+
+export interface FillStyle {
+	color?: string;
+	fillColor?: string;
+}
+
+export interface LineStyle {
+	color?: string;
+	lineColor?: string;
+}
+
+export function getFillStyle(style?: FillStyle): FillLayerPaint {
+	const result: FillLayerPaint = {};
+	if (style) {
+		if (style.fillColor) {
+			result['fill-color'] = Color.parse(style.fillColor).asString();
+		} else if (style.color) {
+			result['fill-color'] = Color.parse(style.color).fade(0.8).asString();
+		}
+	};
+	return result;
+}
+
+export function getLineStyle(style?: LineStyle): LineLayerPaint {
+	const result: LineLayerPaint = {};
+	if (style) {
+		const color = style.lineColor ?? style.color;
+		if (color) result['line-color'] = Color.parse(color).asString();
+	};
+	return result;
+}
