b49dfcb149371eb5889890591ff541ba88276888
Michael Kreil <github@michael-kreil.de>
2025-01-30 02:03:30 +0100

    feat: add editor element list

--- src/lib/components/MapEditor/EditorMarker.svelte
+++ src/lib/components/MapEditor/EditorMarker.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
+	import {} from './editor.scss';
 	import { get } from 'svelte/store';
 	import type { MarkerElement } from './lib/element_marker.js';
 
-	let { element = $bindable() }: { element: MarkerElement } = $props();
+	let { element }: { element: MarkerElement } = $props();
+
+	let color = $state(get(element.color));
+	let size = $state(get(element.size));
+
+	$effect(() => {
+		color = get(element.color);
+		size = get(element.size);
+	});
+
+	$effect(() => element.color.set(color));
+	$effect(() => element.size.set(size));
 </script>
 
 <div class="row">
 	<label for="color-input">Color</label>
-	<input
-		id="color-input"
-		type="color"
-		bind:value={() => get(element.color), (color) => element.color.set(color)}
-	/>
-	<input
-		type="range"
-		min="0"
-		max="5"
-		step="0.1"
-		bind:value={() => get(element.size), (size) => element.size.set(size)}
-	/>
+	<input id="color-input" type="color" bind:value={color} />
+	<label for="size-input">Size</label>
+	<input id="size-input" type="range" min="0" max="5" step="0.1" bind:value={size} />
 </div>
