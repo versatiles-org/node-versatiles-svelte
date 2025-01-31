325ff81fcb1035ce4d769fdeb08ce21e2d72ed15
Michael Kreil <github@michael-kreil.de>
2025-01-31 19:40:25 +0100

    feat: add line element

--- src/lib/components/MapEditor/Editor.svelte
+++ src/lib/components/MapEditor/Editor.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import {} from './editor.scss';
 	import EditorMarker from './EditorMarker.svelte';
+	import EditorLine from './EditorLine.svelte';
 	import type { AbstractElement } from './lib/element_abstract.js';
+	import { LineElement } from './lib/element_line.js';
 	import { MarkerElement } from './lib/element_marker.js';
 
 	const { element }: { element: AbstractElement } = $props();
@@ -21,5 +23,8 @@
 	<input id="input-name" type="text" bind:value={name} />
 	{#if element instanceof MarkerElement}
 		<EditorMarker {element} />
+	{/if}
+	{#if element instanceof LineElement}
+		<EditorLine {element} />
 	{/if}
 {/if}
