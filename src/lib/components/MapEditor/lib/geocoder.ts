41073c7ccec520b759e2dc1c039ad676e729c433
Michael Kreil <github@michael-kreil.de>
2025-01-31 14:40:24 +0100

    refactor: add geocoder

---
+++ src/lib/components/MapEditor/lib/geocoder.ts
@@ -0,0 +1,7 @@
+export async function getLatLng(query: string): Promise<[number, number] | undefined> {
+	const url = `https://photon.komoot.io/api/?${new URLSearchParams(query)}`;
+	const result = await fetch(url).then(r => r.json());
+	const geometry = result?.features?.[0]?.geometry;
+	if (geometry?.type !== 'Point') return
+	return geometry.coordinates;
+};
