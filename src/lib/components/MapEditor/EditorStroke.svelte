a27a8fa7532f012210fd2f85a705f8045ad464e1
Michael Kreil <github@michael-kreil.de>
2025-02-06 20:11:05 +0100

    improve style

--- src/lib/components/MapEditor/EditorStroke.svelte
+++ src/lib/components/MapEditor/EditorStroke.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import {} from './editor.scss';
 	import { get } from 'svelte/store';
 	import { dashArrays, MapLayerLine } from './lib/map_layer/line.js';
 
@@ -17,16 +16,16 @@
 </script>
 
 <div class="row">
-	<label for="dashed-input">Dashed</label>
+	<label for="dashed-input">Dashed:</label>
 	<select id="dashed-input" bind:value={dashed}>
 		{#each dashedList as d}
 			<option value={d}>{d}</option>
 		{/each}
 	</select>
 
-	<label for="color-input">Color</label>
+	<label for="color-input">Color:</label>
 	<input id="color-input" type="color" bind:value={color} />
 
-	<label for="width-input">Width</label>
+	<label for="width-input">Width:</label>
 	<input id="width-input" type="range" min="0.5" max="5" step="0.5" bind:value={width} />
 </div>
--- src/lib/components/MapEditor/EditorSymbol.svelte
+++ src/lib/components/MapEditor/EditorSymbol.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import {} from './editor.scss';
 	import { get } from 'svelte/store';
 	import { MapLayerSymbol, symbols } from './lib/map_layer/symbol.js';
 
@@ -24,24 +23,24 @@
 
 <div class="row">
 	<label for="symbol-input">Symbol</label>
-	<select id="symbol-input" bind:value={symbol}>
+	<select id="symbol-input" bind:value={symbol} class="input">
 		{#each symbolList as s}
 			<option value={s}>{s}</option>
 		{/each}
 	</select>
 
-	<label for="color-input">Color</label>
+	<label for="color-input">Color:</label>
 	<input id="color-input" type="color" bind:value={color} />
 
-	<label for="size-input">Size</label>
+	<label for="size-input">Size:</label>
 	<input id="size-input" type="range" min="0.5" max="3" step="0.1" bind:value={size} />
 
-	<label for="rotate-input">Rotation</label>
+	<label for="rotate-input">Rotation:</label>
 	<input id="rotate-input" type="range" min="-180" max="180" step="15" bind:value={rotate} />
 
-	<label for="halo-input">Halo</label>
+	<label for="halo-input">Halo:</label>
 	<input id="halo-input" type="range" min="0" max="3" step="0.5" bind:value={halo} />
 
-	<label for="label-input">Label</label>
+	<label for="label-input">Label:</label>
 	<input id="label-input" type="text" bind:value={label} />
 </div>
