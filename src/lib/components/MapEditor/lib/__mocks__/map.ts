903ce8b5009532c93f6762de1b1a0363fb6dc132
Michael Kreil <github@michael-kreil.de>
2025-02-09 16:03:51 +0100

    test: cursor

---
+++ src/lib/components/MapEditor/lib/__mocks__/map.ts
@@ -0,0 +1,27 @@
+import { vi } from 'vitest';
+
+export class MockMap {
+	getCanvasContainer = vi.fn(() => ({}));
+	addSource = vi.fn();
+	removeSource = vi.fn();
+	getSource = vi.fn(() => ({ setData: vi.fn() }));
+	addLayer = vi.fn();
+	on = vi.fn();
+	setCenter = vi.fn();
+	setZoom = vi.fn();
+	getZoom = vi.fn(() => 10);
+	getCenter = vi.fn(() => ({ lng: 0, lat: 0 }));
+	queryRenderedFeatures = vi.fn(() => [{ properties: {} }]);
+	setPaintProperty = vi.fn();
+	setLayoutProperty = vi.fn();
+	removeLayer = vi.fn();
+	hasImage = vi.fn();
+	removeImage = vi.fn();
+	addImage = vi.fn();
+	getBounds = vi.fn(() => ({
+		getWest: () => -180,
+		getEast: () => 180,
+		getSouth: () => -90,
+		getNorth: () => 90
+	}))
+}
