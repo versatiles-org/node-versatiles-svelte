b6ec303bb07b9dff81a77f8049bc9a53df18c91b
Michael Kreil <github@michael-kreil.de>
2025-05-05 15:20:56 +0200

    add iframe test

---
+++ src/routes/map-editor/iframe-test/+page.svelte
@@ -0,0 +1,37 @@
+<table>
+	<tbody>
+		<tr>
+			<td></td>
+			<td>
+				<iframe
+					style="width:600px;height:300px;"
+					src="/map-editor#FwAauEhpBq5Ncvv1hOriymXldqFriXkLhIhJieBjBfhDhPLhcPhWhXOiPEkNxSiNrWlCiqAAAAg1Z9TSBrbiRVAAAkZ8IhQBBq98GkJlXEiqAABJWwgyAgGQbu_WFCmLKYMyUWgBOIuVMSKoAAAWUiqAAAA"
+				></iframe></td
+			>
+		</tr>
+		<tr>
+			<td>
+				<iframe
+					style="width:300px;height:600px;"
+					src="/map-editor#FwAauEhpBq5Ncvv1hOriymXldqFriXkLhIhJieBjBfhDhPLhcPhWhXOiPEkNxSiNrWlCiqAAAAg1Z9TSBrbiRVAAAkZ8IhQBBq98GkJlXEiqAABJWwgyAgGQbu_WFCmLKYMyUWgBOIuVMSKoAAAWUiqAAAA"
+				></iframe></td
+			>
+			<td>
+				<iframe
+					style="width:600px;height:600px;"
+					src="/map-editor#FwAauEhpBq5Ncvv1hOriymXldqFriXkLhIhJieBjBfhDhPLhcPhWhXOiPEkNxSiNrWlCiqAAAAg1Z9TSBrbiRVAAAkZ8IhQBBq98GkJlXEiqAABJWwgyAgGQbu_WFCmLKYMyUWgBOIuVMSKoAAAWUiqAAAA"
+				></iframe></td
+			>
+		</tr>
+	</tbody>
+</table>
+
+<style>
+	table {
+		margin: 20px auto;
+	}
+	iframe {
+		border: 1px solid #000;
+		display: block;
+	}
+</style>
