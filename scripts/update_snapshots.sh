90e03ef84051f50285e64ad59bcdc7b9127ab231
Michael Kreil <github@michael-kreil.de>
2025-12-12 09:33:04 +0100

    fix: update snapshot command to use 'all' instead of 'patch'

--- scripts/update_snapshots.sh
+++ scripts/update_snapshots.sh
@@ -3,7 +3,13 @@ set -e
 cd "$(dirname "$0")/.."
 
 docker build . -t playwright-tests -f docker/stable-slim.Dockerfile
-docker run -v $(pwd)/src:/code/src -v $(pwd)/static:/code/static -v $(pwd)/playwright-tests:/code/playwright-tests -v $(pwd)/test-results:/code/test-results --ipc=host --rm playwright-tests /bin/bash -c "npx playwright test --update-snapshots patch"
+docker run \
+  -v $(pwd)/src:/code/src \
+  -v $(pwd)/static:/code/static \
+  -v $(pwd)/playwright-tests:/code/playwright-tests \
+  -v $(pwd)/test-results:/code/test-results \
+  --ipc=host --rm playwright-tests \
+  /bin/bash -c "npx playwright test --update-snapshots all"
 
 # --cache-from type=gha
 # --cache-to type=gha,mode=max
